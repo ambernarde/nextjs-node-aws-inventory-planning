293039115abcd0b9ec45aa3c91e38950d76de8db
Andre Miranda Bernarde <ambernarde@gmail.com>
2024-11-08 10:01:47 -0300

    End of Section 5

--- src/app/state/api.ts
+++ src/app/state/api.ts
@@ -59,15 +59,33 @@ export interface User {
 export const api = createApi({
   baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
   reducerPath: "api",
-  tagTypes:["DashboardMetrics"],
+  tagTypes:["DashboardMetrics", "Products"],
   endpoints:(build) => ({
     getDashboardMetrics : build.query<DashboardMetrics,void>({
       query:() => "/dashboard",
       providesTags:["DashboardMetrics"]
+    }),
+    getProducts:  build.query<Product[],string | void>({
+      query:(search) => ({
+        url: "/products",
+        params: search ? {search} : {}
+      }),
+      providesTags : ["Products"]
+    }),
+    createProduct:  build.mutation<Product,NewProduct>({
+      query:(NewProduct) => ({
+        url: "/products",
+        method:"POST",
+        body: NewProduct
+      }),
+      invalidatesTags: ["Products"] 
     })
+
   }),
 });
 
 export const {
   useGetDashboardMetricsQuery,
+  useGetProductsQuery,
+  useCreateProductMutation
 } = api;
