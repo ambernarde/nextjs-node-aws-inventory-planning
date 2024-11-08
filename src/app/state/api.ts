b8ca10c272a2f183e199d67c25fffcd8bdedf9cb
Andre Miranda Bernarde <ambernarde@gmail.com>
2024-11-08 19:31:32 -0300

    End of Settings Page

--- src/app/state/api.ts
+++ src/app/state/api.ts
@@ -1,3 +1,4 @@
+
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export interface Product {
@@ -59,7 +60,7 @@ export interface User {
 export const api = createApi({
   baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
   reducerPath: "api",
-  tagTypes:["DashboardMetrics", "Products"],
+  tagTypes:["DashboardMetrics", "Products","Users"],
   endpoints:(build) => ({
     getDashboardMetrics : build.query<DashboardMetrics,void>({
       query:() => "/dashboard",
@@ -79,7 +80,11 @@ export const api = createApi({
         body: NewProduct
       }),
       invalidatesTags: ["Products"] 
-    })
+    }),
+    getUsers : build.query<User[],void>({
+      query:() => "/users",
+      providesTags:["Users"]
+    }),
 
   }),
 });
@@ -87,5 +92,6 @@ export const api = createApi({
 export const {
   useGetDashboardMetricsQuery,
   useGetProductsQuery,
-  useCreateProductMutation
+  useCreateProductMutation,
+  useGetUsersQuery,  
 } = api;
